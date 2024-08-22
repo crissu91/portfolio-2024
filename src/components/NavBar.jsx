02fbba0257bed912b162901a98bc3e4e0895ddfb
crissu91 <cristinaf.stefan@gmail.com>
2024-08-22 15:33:42 +0100

    Remove navbar image on small screen and replace logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import logo from "../assets/cristina-logo.png";
-import logo_short from "../assets/cristina-logo-short.png";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function NavBar() {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
+    <nav className="mb-28 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <img
           src={logo}
           alt="Cristina Stefan name printed in gold"
-          className="hidden md:block w-[300px] h-auto transition-opacity"
-        />
-        <img
-          src={logo_short}
-          alt="Cristina Stefan name printed in gold"
-          className="md:hidden w-[150px] h-auto"
+          className="w-fit h-[50px] md:h-[75px] transition-opacity"
         />
       </div>
 
