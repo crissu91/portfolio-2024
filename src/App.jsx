039a5f6345537ba2214e101bb0f8c65d27c6e763
crissu91 <cristinaf.stefan@gmail.com>
2024-08-22 14:07:58 +0100

    Create NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="overflow-x-clip text-neutral-200 antialiased selection:bg-yellow-300 selection:text-yellow-900">
+      <div className="fixed top-0 -z-10 h-full w-full"></div>
+
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+      <div className="container mx-auto px-8">
+        <NavBar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import logo from "../assets/cristina-logo.png";
+import logo_short from "../assets/cristina-logo-short.png";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img
+          src={logo}
+          alt="Cristina Stefan name printed in gold"
+          className="hidden md:block w-[300px] h-auto transition-opacity"
+        />
+        <img
+          src={logo_short}
+          alt="Cristina Stefan name printed in gold"
+          className="md:hidden w-[150px] h-auto"
+        />
+      </div>
+
+      <div className="m-8 flex items-center gap-4 justify-center text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+      </div>
+    </nav>
+  );
 }
