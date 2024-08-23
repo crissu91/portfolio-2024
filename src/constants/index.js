3338571a622b101a0e60353d6e1e38dd2afe1ad3
crissu91 <cristinaf.stefan@gmail.com>
2024-08-23 12:06:19 +0100

    Add new project

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ import nc_news from "../assets/projects/nc-news.png";
 import weather_app from "../assets/projects/weather_app.png";
 import safer_places from "../assets/projects/safer-places.png";
 import redmapp from "../assets/projects/redmapp.png";
+import gttg from "../assets/projects/gttg.png";
 
 export const HERO_CONTENT = `Adept at combining technical expertise with strong interpersonal and transferable skills, including collaboration, problem-solving, and effective communication. Recognized for exceptional analytical skills, a collaborative approach, and a passion for continuous improvement and learning.`;
 
@@ -12,31 +13,10 @@ export const EXPERIENCES = [];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
-    image: ecommerce_shop,
-    description:
-      "A fully functional e-commerce website with features like product listing and shopping cart.",
-    technologies: ["TailwindCSS", "React", "FakeStoreApi"],
-  },
-  {
-    title: "Weather App",
-    image: weather_app,
-    description:
-      "An application for getting temperature, max temperature, min temperature, humidity, real feel, sunrise, sunset, hourly and daily forecast. Is it also displaying the local time at the selected location.",
-    technologies: ["HTML", "TailwindCSS", "React", "Unicons"],
-  },
-  {
-    title: "News Website",
-    image: nc_news,
-    associated_with: "Northcoders",
-    description:
-      "A news website. Each article can be voted using the API. Users can also add comments about an article, log in, sign up and see their account details.A user can remove their own comments. All data come from own implementation of the API server.",
-    technologies: ["HTML", "CSS", "React", "Node.js"],
-  },
-  {
     title: "Safer Places Website",
     image: safer_places,
     associated_with: "Few and Far",
+    link: "https://www.saferplaces.co.uk/",
     description:
       "A platform for supporting all adult & child victims of domestic abuse across Essex & Hertfordshire.",
     technologies: ["TailwindCSS", "Vue.js", "PHP", "Laravel", "Statamic"],
@@ -45,9 +25,44 @@ export const PROJECTS = [
     title: "Redmapp Website",
     image: redmapp,
     associated_with: "Few and Far",
+    link: "https://www.redmapp.org.uk/",
     description:
-      "A platform for supporting all to parents when making treatment decisions for their child with relapsed or refractory neuroblastoma in the United Kingdom.",
+      "A platform for supporting parents when making treatment decisions for their child with relapsed or refractory neuroblastoma in the United Kingdom.",
     technologies: ["TailwindCSS", "Vue.js", "PHP", "Laravel", "Statamic"],
+  },
+  {
+    title: "GTTG Website",
+    image: gttg,
+    associated_with: "Few and Far",
+    link: "https://www.gettogetherthroughgaming.com/",
+    description:
+      "A platform for a gaming event supporting better mental health for all. In partnership with Leeds Mind.",
+    technologies: ["TailwindCSS", "Vue.js", "PHP", "Laravel", "Statamic"],
+  },
+  {
+    title: "News Website",
+    image: nc_news,
+    associated_with: "Northcoders",
+    link: "https://crissu-nc-news.netlify.app/",
+    description:
+      "A news website. Each article can be voted using the API. Users can also add comments about an article, log in, sign up and see their account details.A user can remove their own comments. All data come from own implementation of the API server.",
+    technologies: ["HTML", "CSS", "React", "Node.js"],
+  },
+  {
+    title: "E-Commerce Website",
+    image: ecommerce_shop,
+    link: "https://crissu91-ecommerce-shop.netlify.app/",
+    description:
+      "A fully functional e-commerce website with features like product listing and shopping cart.",
+    technologies: ["TailwindCSS", "React", "FakeStoreApi"],
+  },
+  {
+    title: "Weather App",
+    image: weather_app,
+    link: "https://crissu91-weather-app.netlify.app/",
+    description:
+      "An application for getting temperature, max temperature, min temperature, humidity, real feel, sunrise, sunset, hourly and daily forecast. Is it also displaying the local time at the selected location.",
+    technologies: ["HTML", "TailwindCSS", "React", "Unicons"],
   },
 ];
 
