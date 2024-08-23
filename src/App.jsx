3871e69de4f68c77412a6b04b72867f29de5bda3
crissu91 <cristinaf.stefan@gmail.com>
2024-08-23 12:06:38 +0100

    Add Projects component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import TechStack from "./components/TechStack";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -10,11 +11,12 @@ function App() {
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
 
-      <div className="container mx-auto px-8">
+      <div className="container mx-auto px-10 md:px-20">
         <NavBar />
         <Hero />
         <About />
         <TechStack />
+        <Projects />
       </div>
     </div>
   );
