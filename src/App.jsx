1f24b08c21284de12fa8fc3deeeb3ea0db537962
crissu91 <cristinaf.stefan@gmail.com>
2024-08-23 13:22:59 +0100

    Add Contact component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import TechStack from "./components/TechStack";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <About />
         <TechStack />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
