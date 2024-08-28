2c93026e6c21549f475adf406ac7ccdeea690379
crissu91 <cristinaf.stefan@gmail.com>
2024-08-28 19:33:50 +0100

    Create LazyLoad component

---
+++ src/components/LazyLoad.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const LazyLoad = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    observer.observe(element);
+
+    return () => {
+      if (element) {
+        observer.unobserve(element);
+      }
+    };
+  }, []);
+
+  const enhancedChildren = React.cloneElement(children, { isVisible });
+
+  return <div ref={ref}>{enhancedChildren}</div>;
+};
+
+export default LazyLoad;
